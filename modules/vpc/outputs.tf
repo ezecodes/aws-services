09d0620be12e4aa4e41615b511fdb23f8ca5cec9
jahdevelops <elijaheze777@gmail.com>
2024-03-01 20:43:01 +0100

    define outputs for vpc module

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,15 @@
+output "vpc_id" {
+  value = aws_vpc.vpc.id
+}
+output "route_table_id" {
+  value = aws_route_table.route_table.id
+}
+output "primary_public_subnet_id" {
+  value = aws_subnet.primary_public_subnet
+}
+output "secondary_public_subnet_id" {
+  value = aws_subnet.secondary_public_subnet
+}
+output "internet_gateway_id" {
+  value = aws_internet_gateway.internet_gateway.id
+}
