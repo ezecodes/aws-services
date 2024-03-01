1f706b732d9efa298a500b1ca632305bf7de75e3
jahdevelops <elijaheze777@gmail.com>
2024-03-01 20:43:29 +0100

    define variables for vpc module

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {}
+variable "cidr_primary_public" {}
+variable "cidr_secondary_public" {}
+
+variable "cidr_primary_private" {}
+variable "cidr_secondary_private" {}
+
+variable "project_name" {}
+variable "vpc_cidr" {}
