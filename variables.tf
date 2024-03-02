f60ceee0a089f9e6b4dee195b23e8ab92735aaac
jahdevelops <elijaheze777@gmail.com>
2024-03-02 03:28:05 +0100

    add root vars

--- variables.tf
+++ variables.tf
@@ -2,3 +2,11 @@ variable "aws_region" {}
 variable "project_name" {}
 variable "mysql_user" {}
 variable "mysql_password" {}
+
+
+variable "cidr_vpc" {}
+variable "cidr_private_primary_subnet" {}
+variable "cidr_private_secondary_subnet" {}
+variable "cidr_public_primary_subnet" {}
+variable "cidr_public_secondary_subnet" {}
+variable "cidr_route_table" {}
