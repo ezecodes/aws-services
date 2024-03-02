6c2bc9350d466751cb7c6ffe48a2f552f049d091
jahdevelops <elijaheze777@gmail.com>
2024-03-02 03:17:36 +0100

    define database module

---
+++ modules/mysql/main.tf
@@ -0,0 +1,22 @@
+module "vpc" {
+  source = "../vpc"
+  project_name = var.project_name
+}
+
+resource "aws_db_subnet_group" "db_subnets" {
+  name = "db_subnets"
+  subnet_ids = [module.vpc.primary_private_subnet_id, module.vpc.secondary_private_subnet_id]
+}
+
+resource "aws_db_instance" "mysql" {
+  allocated_storage    = 10
+  db_name              = "${var.project_name}_mysqldb"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = var.mysql_user
+  password             = var.mysql_password
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  db_subnet_group_name = aws_db_subnet_group.db_subnets.name
+}
