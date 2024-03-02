40f22edfa067659148930bd82405b2910003f688
jahdevelops <elijaheze777@gmail.com>
2024-03-02 03:27:26 +0100

    define vpc variables

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,1 +1,7 @@
 variable "project_name" {}
+variable "cidr_vpc" {}
+variable "cidr_private_primary_subnet" {}
+variable "cidr_private_secondary_subnet" {}
+variable "cidr_public_primary_subnet" {}
+variable "cidr_public_secondary_subnet" {}
+variable "cidr_route_table" {}
