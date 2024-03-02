cd168a60b31fe32f518def8d4e5383923f79e87c
jahdevelops <elijaheze777@gmail.com>
2024-03-02 03:10:50 +0100

    expose private subnet ids

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -10,6 +10,12 @@ output "primary_public_subnet_id" {
 output "secondary_public_subnet_id" {
   value = aws_subnet.secondary_public_subnet
 }
+output "primary_private_subnet_id" {
+  value = aws_subnet.primary_private_subnet
+}
+output "secondary_private_subnet_id" {
+  value = aws_subnet.secondary_private_subnet
+}
 output "internet_gateway_id" {
   value = aws_internet_gateway.internet_gateway.id
 }
