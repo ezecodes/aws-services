0934a9838d4bb52cc50e18cb5bd14a750a4dd20f
jahdevelops <elijaheze777@gmail.com>
2024-03-02 03:11:31 +0100

    delete unused vars

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,9 +1,1 @@
-variable "aws_region" {}
-variable "cidr_primary_public" {}
-variable "cidr_secondary_public" {}
-
-variable "cidr_primary_private" {}
-variable "cidr_secondary_private" {}
-
 variable "project_name" {}
-variable "vpc_cidr" {}
