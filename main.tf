b2890e6033549105394dc4a8ea7aed4da299fd1a
jahdevelops <elijaheze777@gmail.com>
2024-03-02 03:27:12 +0100

    update vpc inputs

--- main.tf
+++ main.tf
@@ -4,6 +4,12 @@ provider "aws" {
 module "vpc" {
   source = "./modules/vpc"
   project_name = var.project_name
+  cidr_vpc = var.cidr_vpc
+  cidr_private_primary_subnet = var.cidr_private_primary_subnet
+  cidr_private_secondary_subnet = var.cidr_private_secondary_subnet
+  cidr_public_primary_subnet = var.cidr_public_primary_subnet
+  cidr_public_secondary_subnet = var.cidr_public_secondary_subnet
+  cidr_route_table = var.cidr_route_table
 }
 module "mysql" {
   source = "./modules/mysql"
