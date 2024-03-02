d55c272409906aa36a5331a2347537768ccc274e
jahdevelops <elijaheze777@gmail.com>
2024-03-02 18:20:29 +0100

    format ec2 config

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "vpc" {
-  cidr_block = var.cidr_vpc
-  instance_tenancy = "default"
+  cidr_block           = var.cidr_vpc
+  instance_tenancy     = "default"
   enable_dns_hostnames = true
   tags = {
     Name = "${var.project_name}-vpc"
@@ -19,37 +19,37 @@ resource "aws_internet_gateway" "internet_gateway" {
 }
 
 resource "aws_subnet" "primary_public_subnet" {
-  vpc_id = aws_vpc.vpc.id
-  cidr_block = var.cidr_public_primary_subnet
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.cidr_public_primary_subnet
   map_public_ip_on_launch = true
-  availability_zone = data.aws_availability_zones.available_zones.names[0]
+  availability_zone       = data.aws_availability_zones.available_zones.names[0]
   tags = {
     Name = "${var.project_name}-public_subnets"
   }
 }
 resource "aws_subnet" "secondary_public_subnet" {
-  vpc_id = aws_vpc.vpc.id
-  cidr_block = var.cidr_public_secondary_subnet
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.cidr_public_secondary_subnet
   map_public_ip_on_launch = true
-  availability_zone = data.aws_availability_zones.available_zones.names[1]
+  availability_zone       = data.aws_availability_zones.available_zones.names[1]
   tags = {
     Name = "${var.project_name}-public_subnets"
   }
 }
 resource "aws_subnet" "primary_private_subnet" {
-  vpc_id = aws_vpc.vpc.id
-  cidr_block = var.cidr_private_primary_subnet
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.cidr_private_primary_subnet
   map_public_ip_on_launch = false
-  availability_zone = data.aws_availability_zones.available_zones.names[0]
+  availability_zone       = data.aws_availability_zones.available_zones.names[0]
   tags = {
     Name = "${var.project_name}-private_subnets"
   }
 }
 resource "aws_subnet" "secondary_private_subnet" {
-  vpc_id = aws_vpc.vpc.id
-  cidr_block = var.cidr_private_secondary_subnet
+  vpc_id                          = aws_vpc.vpc.id
+  cidr_block                      = var.cidr_private_secondary_subnet
   map_customer_owned_ip_on_launch = false
-  availability_zone = data.aws_availability_zones.available_zones.names[1]
+  availability_zone               = data.aws_availability_zones.available_zones.names[1]
   tags = {
     Name = "${var.project_name}-private_subnets"
   }
@@ -67,9 +67,9 @@ resource "aws_route_table" "route_table" {
 
 resource "aws_route_table_association" "primary_route_table_assoc" {
   route_table_id = aws_route_table.route_table.id
-  subnet_id = aws_subnet.primary_public_subnet.id
+  subnet_id      = aws_subnet.primary_public_subnet.id
 }
 resource "aws_route_table_association" "secondary_route_table_assoc" {
   route_table_id = aws_route_table.route_table.id
-  subnet_id = aws_subnet.secondary_public_subnet.id
+  subnet_id      = aws_subnet.secondary_public_subnet.id
 }
